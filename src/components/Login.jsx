03eb0cb3e408f4fb7b1b1da5e1a9860c1d91424b
Mishael <mishaelclifford1@gmail.com>
2023-08-26 13:23:49 +0100

    Adding firebase

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,56 +1,23 @@
-import axios from "axios";
 import React from "react";
-import { useState } from "react";
 import { CgProfile } from "react-icons/cg";
 import { FaLock } from "react-icons/fa";
-import { Link, useNavigate } from "react-router-dom";
+// import { Link, useNavigate } from "react-router-dom";
 function Login() {
-  const history = useNavigate();
-  const { email, setEmail } = useState("");
-  const { password, setPassword } = useState("");
-
-  async function submit(e) {
-    e.preventDefault();
-
-    try {
-      await axios.post("http://localhost:8000/"),
-        {
-          email,
-          password,
-        }
-
-          .then((res) => {
-            if (res.data == "exist") {
-              history("/dashboard", { state: { id: email } });
-            } else if (res.data == "notexist") {
-              alert("User have not signed up");
-            }
-          })
-          .catch((e) => {
-            alert("wrong details");
-            console.log(e);
-          });
-    } catch (e) {
-      console.log(e);
-    }
+  const handleSumit=(e)=>{
+    e.preventDefault()
+    console.log(e.target.email.value)
   }
-
   return (
-    <div className="flex flex-col items-center justify-center h-screen   ">
-      <div className="w-96 p-6 shadow-lg bg-white space-y-8 rounded-md">
-        <p class="text-3xl block text-center font-semibold">Sign In</p>
+    <div className="flex flex-col items-center justify-center">
+      <div className=" p-6 md:w-[34%] sm:w-[10%] shadow-lg bg-white space-y-8 rounded-md">
+        <p className="text-3xl block text-center font-semibold">Sign In</p>
         <div className="flex flex-col items-center justify-center space-y-2 ">
-          <form action="POST">
+          <form onSubmit={(e)=>handleSumit(e)}>
             <div className="flex items-center border rounded-md px-2 text-2xl font-semibold">
               <CgProfile size={20} />
               <input
-                placeholder="Username or email"
+                placeholder="Email"
                 type="email"
-                onChange={(e) => {
-                  setEmail(e.target.value);
-                }}
-                name=""
-                id=""
                 className=" w-full h-[50px] text-base px-2 py-1 focus:outline-none focus:ring-0 focus:border-gray-600"
               />
             </div>
@@ -59,53 +26,29 @@ function Login() {
               <input
                 placeholder="Password"
                 type="password"
-                onChange={(e) => {
-                  setPassword(e.target.value);
-                }}
-                name=""
-                id=""
                 className=" w-full h-[50px] text-base px-2 py-1 focus:outline-none focus:ring-0 focus:border-gray-600 "
               />
             </div>
-          </form>
-        </div>
-        <div className="flex mt-3 justify-between items-center">
-          <div>
-            <input type="checkbox" />
-            <label>Remember Me?</label>
-          </div>
-          <p class="text-slate-500">Forgot Password</p>
-        </div>
-        <Link to="/Dashboard">
+        {/* <Link to="/dashboard"> */}
           <div className="mt-5 ">
-            <button
+            <button 
               type="submit"
-              onClick={submit}
-              class="border-2 border-[#37a137] rounded-lg bg-[#37a137] text-white hover:text-slate-600 hover:bg-transparent text-xl font-semibold py-1 w-full"
+              className="border-2 border-[#37a137] rounded-lg bg-[#37a137] text-white hover:text-slate-600 hover:bg-transparent text-xl font-semibold py-1 w-full"
             >
               Sign in
             </button>
           </div>
-        </Link>
-        <Link to="/signup">
-          <div className="mt-5 ">
-            <button
-              type="submit"
-              class="border-2 border-[#e2424a] rounded-lg bg-[#e2424a] text-white hover:text-slate-600 hover:bg-transparent text-xl font-semibold py-1 w-full"
-            >
-              Sign up
-            </button>
-          </div>
-        </Link>
-
-        <p class="flex justify-center items-center">Or</p>
+        {/* </Link> */}
+          </form>
+        </div>
+        <p className="flex justify-center items-center">Or</p>
         {/* Google & Facebook */}
         <div className="p-6 ">
-          <div className="flex flex-row items-center justify-between space-x-10">
-            <div className="flex  mt-5 items-center w-full bg-[#f31111] rounded-lg hover:bg-transparent px-5">
+          <div className="flex md:flex-row sm:flex-col justify-center items-center md:space-x-10">
+            <div className="flex  mt-5 items-start w-full bg-[#f31111] rounded-lg hover:bg-transparent px-5">
               <button
                 type="submit"
-                class="text-white hover:text-slate-600 text-xl font-semibold  py-1 w-full"
+                className="text-white hover:text-slate-600 text-xl font-semibold  py-1 w-full"
               >
                 Google
               </button>
@@ -114,18 +57,18 @@ function Login() {
             <div className="flex  mt-5 items-center w-full bg-[#160674da] rounded-lg hover:bg-transparent px-5">
               <button
                 type="submit"
-                class="text-white hover:text-slate-600 text-xl font-semibold  py-1 w-full"
+                className="text-white hover:text-slate-600 text-xl font-semibold  py-1 w-full"
               >
                 Facebook
               </button>
             </div>
           </div>
           {/* Linledin & GitHub */}
-          <div className="flex flex-row items-center justify-between space-x-10">
+          <div className="flex md:flex-row sm:flex-col items-center justify-center md:space-x-10">
             <div className="flex  mt-5 items-center w-full bg-[#336492] rounded-lg hover:bg-transparent px-5">
               <button
                 type="submit"
-                class="text-white hover:text-slate-600 text-xl font-semibold  py-1 w-full"
+                className="text-white hover:text-slate-600 text-xl font-semibold  py-1 w-full"
               >
                 Linkedin
               </button>
@@ -135,7 +78,7 @@ function Login() {
             <div className="flex  mt-5 items-center w-full bg-[#1b1a1a] rounded-lg hover:bg-transparent px-5">
               <button
                 type="submit"
-                class="text-white hover:text-slate-600 text-xl font-semibold  py-1 w-full"
+                className="text-white hover:text-slate-600 text-xl font-semibold  py-1 w-full"
               >
                 GitHub
               </button>
@@ -143,7 +86,7 @@ function Login() {
           </div>
         </div>
         <div className="flex flex-col items-center justify-center">
-          <p class="text-xl font-semibold text-[#37a137] hover:underline cursor-pointer">
+          <p className="text-xl font-semibold text-[#37a137] hover:underline cursor-pointer">
             Why Create an Account?
           </p>
         </div>
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,42 +1,11 @@
-import axios from "axios";
 import React from "react";
-import { useState } from "react";
 import { CgProfile } from "react-icons/cg";
 import { FaLock } from "react-icons/fa";
 import { Link } from "react-router-dom";
-function Signup() {
-  const { email, setEmail } = useState("");
-  const { password, setPassword } = useState("");
-
-  async function submit(e) {
-    e.preventDefault();
-
-    try {
-      await axios.post("http://localhost:8000/"),
-        {
-          email,
-          password,
-        }
-
-          .then((res) => {
-            if (res.data == "exist") {
-              alert("User already exist");
-            } else if (res.data == "notexist") {
-              history("/dashboard", { state: { id: email } });
-            }
-          })
-          .catch((e) => {
-            alert("wrong details");
-            console.log(e);
-          });
-    } catch (e) {
-      console.log(e);
-    }
-  }
-
+function Sign_up() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen   ">
-      <div className="w-96 p-6 shadow-lg bg-white space-y-8 rounded-md">
+    <div className="flex flex-col items-center justify-center">
+      <div className=" p-6 md:w-[34%] sm:w-[10%] shadow-lg bg-white space-y-8 rounded-md">
         <p class="text-3xl block text-center font-semibold">Sign Up</p>
         <div className="flex flex-col items-center justify-center space-y-2 ">
           <form action="POST">
@@ -45,9 +14,6 @@ function Signup() {
               <input
                 placeholder="Username or email"
                 type="email"
-                onChange={(e) => {
-                  setEmail(e.target.value);
-                }}
                 name=""
                 id=""
                 className=" w-full h-[50px] text-base px-2 py-1 focus:outline-none focus:ring-0 focus:border-gray-600"
@@ -58,34 +24,28 @@ function Signup() {
               <input
                 placeholder="Password"
                 type="password"
-                onChange={(e) => {
-                  setPassword(e.target.value);
-                }}
                 name=""
                 id=""
                 className=" w-full h-[50px] text-base px-2 py-1 focus:outline-none focus:ring-0 focus:border-gray-600 "
               />
             </div>
-          </form>
-        </div>
-
-        <Link to="/Dashboard">
+            <Link to="/dashboard">
           <div className="mt-5 ">
             <button
               type="submit"
-              onClick={submit}
               class="border-2 border-[#37a137] rounded-lg bg-[#37a137] text-white hover:text-slate-600 hover:bg-transparent text-xl font-semibold py-1 w-full"
             >
               Sign up
             </button>
           </div>
         </Link>
-
+          </form>
+        </div>
         <p class="flex justify-center items-center">Or</p>
-        {/* Google & Facebook */}
-        <div className="p-6 ">
-          <div className="flex flex-row items-center justify-between space-x-10">
-            <div className="flex  mt-5 items-center w-full bg-[#f31111] rounded-lg hover:bg-transparent px-5">
+         {/* Google & Facebook */}
+         <div className="p-6 ">
+          <div className="flex md:flex-row sm:flex-col justify-center items-center md:space-x-10">
+            <div className="flex  mt-5 items-start w-full bg-[#f31111] rounded-lg hover:bg-transparent px-5">
               <button
                 type="submit"
                 class="text-white hover:text-slate-600 text-xl font-semibold  py-1 w-full"
@@ -104,7 +64,7 @@ function Signup() {
             </div>
           </div>
           {/* Linledin & GitHub */}
-          <div className="flex flex-row items-center justify-between space-x-10">
+          <div className="flex md:flex-row sm:flex-col items-center justify-center md:space-x-10">
             <div className="flex  mt-5 items-center w-full bg-[#336492] rounded-lg hover:bg-transparent px-5">
               <button
                 type="submit"
@@ -135,4 +95,4 @@ function Signup() {
   );
 }
 
-export default Signup;
+export default Sign_up;
